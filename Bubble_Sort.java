2765885ef5c67adec7e18e2dc4ef5dc583b36ff0
utkarsh2603 <utkarsh70888@gmail.com>
2023-01-31 12:10:02 +0530

    Initial Commit

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,2 +1,31 @@
-public class Bubble_Sort {
+public class Bubble_Sort
+{
+    public static void bubbleSort(int array[])
+    {
+        for(int i=0;i< array.length-1;i++)
+        {
+            for(int j=0;j< array.length-1-i;j++)
+            {
+                if(array[j]>array[j+1])
+                {
+                    int temp=array[j+1];
+                    array[j+1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+    }
+    public static void print_array(int array[])
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int array[]={3,5,2,6,1,9,8,4,7};
+
+        bubbleSort(array);
+        print_array(array);
+    }
 }
